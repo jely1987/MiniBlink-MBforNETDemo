89c55bcc91f25e20a31e3dbae56feb440f9e6d92
Ampere <ampereufo@qq.com>
2020-07-03 19:47:30 +0800

    添加项目文件。

---
+++ HookNetwork/Form1.cs
@@ -0,0 +1,29 @@
+﻿using MB;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HookNetwork
+{
+    public partial class Form1 : Form
+    {
+        WebView m_wView = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m_wView = new WebView();
+            m_wView.Bind(this);
+            m_wView.LoadURL("https://www.baidu.com/");
+        }
+    }
+}
