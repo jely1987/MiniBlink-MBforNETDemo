3fa9d62a766b6a04d7a0fd111fa64bb44278063e
Ampere <ampereufo@qq.com>
2020-07-19 02:14:10 +0800

    删掉多余资源

--- WebUserface/Properties/Resources.Designer.cs
+++ WebUserface/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace WebUserface.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
-        /// </summary>
-        internal static System.Drawing.Bitmap chrome {
-            get {
-                object obj = ResourceManager.GetObject("chrome", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
